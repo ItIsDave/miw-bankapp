2fd6b73c3c8e993e0460c9862e4e9fdd7418026a
Adamantia <triantou.adamantia@hva.nl>
2019-08-23 23:01:19 +0200

    added database connection model and controller

---
+++ couch/src/main/java/controller/ConnectionLauncher.java
@@ -0,0 +1,30 @@
+package controller;
+
+
+import model.dao.DBaccess;
+
+public class ConnectionLauncher {
+
+    private DBaccess db;
+    private static final String DASHED_LINE = "------------------------";
+
+    public ConnectionLauncher() {
+        super();
+        db = new DBaccess();
+
+    }
+
+    public void run() {
+
+        try {
+            db.openConnection();
+            System.out.println("Connection open");
+            System.out.println(DASHED_LINE);
+        } catch (Exception e) {
+            System.out.println("\nEr is iets fout gegaan\n");
+            e.printStackTrace();
+        }
+
+        db.closeConnection();
+    }
+}
