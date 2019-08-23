3f76bb2f2edc96429ce279a08d8deff4536adc55
David Peek <david.peek@hva.nl>
2019-08-23 15:18:08 +0200

    Eerste commit

---
+++ couch/src/test/java/miw/s16/couch/couch/CouchApplicationTests.java
@@ -0,0 +1,16 @@
+package miw.s16.couch.couch;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CouchApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
