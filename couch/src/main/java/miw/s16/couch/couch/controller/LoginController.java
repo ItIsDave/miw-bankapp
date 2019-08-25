e9fc513a1acf2882051dd3169776ec33eef8413c
Adamantia <triantou.adamantia@hva.nl>
2019-08-25 17:18:06 +0200

    added first template to login controller

--- couch/src/main/java/miw/s16/couch/couch/controller/LoginController.java
+++ couch/src/main/java/miw/s16/couch/couch/controller/LoginController.java
@@ -3,7 +3,9 @@ package miw.s16.couch.couch.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -16,18 +18,18 @@ public class LoginController {
      * @author adamantia
      */
 
-    @RequestMapping("/insert")
-    public String insertData(){
-        // in our database userId has to autoincrement, this is for testing only
-        jdbc.execute("insert into user(userName,userPassword, idUser)values('userTest','1234', 10)");
-        return"successful_entry";
-    }
-
-    //    @RequestMapping(value ="welkom", method = RequestMethod.GET)
-//    public String welkomHandler(@RequestParam String naam, Model model) {
-//        model.addAttribute("naam", naam);
-//        return "login_template";
+//    @RequestMapping("/insert")
+//    public String insertData(){
+//        // in our database userId has to autoincrement, this is for testing only
+//        jdbc.execute("insert into user(userName,userPassword, idUser)values('userTest','1234', 10)");
+//        return"successful_entry";
 //    }
+
+    @RequestMapping(value="couch", method= RequestMethod.GET)
+    public String appHandler(Model model){
+        model.addAttribute("welkom", "Welkom bij Couch Bank");
+        return "welcome_template";
+    }
 }
 
 
