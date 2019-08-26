51eb4f87ca16a1f07c54ce9bed59756cb6f5d991
Adamantia <triantou.adamantia@hva.nl>
2019-08-26 10:33:59 +0200

    added model user class

--- couch/src/main/java/miw/s16/couch/couch/model/User.java
+++ couch/src/main/java/miw/s16/couch/couch/model/User.java
@@ -6,4 +6,42 @@ public class User {
     private String userName;
     private String userPassword;
     private int userId;
+
+
+
+    public User() {
+        this("", "", 0);
+    }
+
+
+    public User(String userName, String userPassword, int userId) {
+        super();
+        this.userName = userName;
+        this.userPassword = userPassword;
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
