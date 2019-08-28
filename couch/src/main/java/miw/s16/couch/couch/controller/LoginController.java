4350eb8381a15529e5c1116dbbaacdda70eb2bb7
Alet <aletvdvoorn@gmail.com>
2019-08-28 12:18:23 +0200

    Alet - Klasse Bank, SME, Retail,LoginCongtroller incl testUserObjecten

--- couch/src/main/java/miw/s16/couch/couch/controller/LoginController.java
+++ couch/src/main/java/miw/s16/couch/couch/controller/LoginController.java
@@ -1,5 +1,8 @@
 package miw.s16.couch.couch.controller;
 
+import miw.s16.couch.couch.model.BankUser;
+import miw.s16.couch.couch.model.RetailUser;
+import miw.s16.couch.couch.model.SMEUser;
 import miw.s16.couch.couch.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -44,9 +47,28 @@ public class LoginController {
 
         // test user for login functionality
         User testUser = new User("Test", "1234", 140);
+        BankUser bankuser = new BankUser("userBankuser", "pwBankUser", 23, "hoofdMKB");
+        SMEUser smeuser = new SMEUser("userSmeuser", "pwSmeUser", 23, 23, "Aliebalie", "baliemedewerker");
+        RetailUser retailuser = new RetailUser("userRetailUser", "pwRetailUser",23,
+        23, "Alie", "", "Balie",
+                "retailstraat", 23, "",
+                "23", "Retail", 23,
+        23, "oeps", "eigenaar");
 
         if (testUser != null && testUser.getUserPassword().equals(user.getUserPassword())) {
             model.addAttribute("name", testUser.getUserName());
+            return "personal_page";
+        }
+        if (bankuser != null && bankuser.getUserPassword().equals(user.getUserPassword())) {
+            model.addAttribute("name", bankuser.getUserName());
+            return "personal_page";
+        }
+        if (smeuser != null && smeuser.getUserPassword().equals(user.getUserPassword())) {
+            model.addAttribute("name", smeuser.getUserName());
+            return "personal_page";
+        }
+        if (retailuser != null && retailuser.getUserPassword().equals(user.getUserPassword())) {
+            model.addAttribute("name", retailuser.getUserName());
             return "personal_page";
         }
         return"login failed";
