7589587d19c54acc6968ecb3945418f576d2cd13
Adamantia <triantou.adamantia@hva.nl>
2019-09-11 12:12:28 +0200

    put code in the corresponding controller to avoid confusion

--- couch/src/main/java/miw/s16/couch/couch/controller/TransactionController.java
+++ couch/src/main/java/miw/s16/couch/couch/controller/TransactionController.java
@@ -6,6 +6,7 @@ import miw.s16.couch.couch.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -24,6 +25,31 @@ public class TransactionController {
     TransactionService transactionService;
 
     BankAccount accountTo = new BankAccount();
+
+
+    // if user chooses to make a new transaction
+    @GetMapping(value = "transactionRequest")
+    public String pageHandlerGet(@ModelAttribute User user, Model model, HttpServletRequest request) {
+        // log in session
+        Transaction transaction = new Transaction();
+        HttpSession session = request.getSession (true);
+        String userName = (String) session.getAttribute("userName");
+        RetailUser retailUser1  = (RetailUser) session.getAttribute("retailUser");
+        int userId = (int) session.getAttribute("userId");
+        BankAccount bankAccountFrom = retailUser1.getBankAccounts().get(0);
+
+        transaction.setBankAccount(accountTo);
+        transaction.setFromAccount(accountTo.getIBAN());
+        System.out.println("datum - tijd is: " + transaction.getTransactionDate().toString());
+        model.addAttribute("transaction", transaction);
+        model.addAttribute("date_time", transaction.getTransactionDate().toString());
+        model.addAttribute("bankAccountFrom", bankAccountFrom.getIBAN());
+        model.addAttribute("bankAccountTo", transaction.getToAccount());
+        model.addAttribute("userName", userName);
+        model.addAttribute("user", user);
+        model.addAttribute("balance", bankAccountFrom.getBalance());
+        return "transaction";
+    }
 
 
     @PostMapping(value="transactionConfirmation")
